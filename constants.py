7d3abb848291cae97da7009adf044d4cb3241042
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2023-07-18 21:37:38 -0600

    constants file updated to be based on environment

--- constants.py
+++ constants.py
@@ -1,8 +1,55 @@
-USER = 'sa'
-PASSWORD = '(oolFrog45!!'
-HOST = '127.0.0.1'
-PORT = '1433'
-DATABASE = 'pokerflowDB'
+import boto3
+from botocore.exceptions import ClientError
+import json
+import os
 
+
+DATABASE = 'pokerflowDB'
 DRIVER = 'ODBC+Driver+18+for+SQL+Server'
 TRUST = 'TrustServerCertificate=yes'
+ENV = os.getenv('env')
+
+if not ENV:
+  raise Exception('Missing environment variable: env')
+
+if ENV == 'prod':
+  secret_name = "pokerflow-database-1"
+  region_name = "us-east-2"
+
+  # Create a Secrets Manager client
+  session = boto3.session.Session()
+  client = session.client(
+      service_name='secretsmanager',
+      region_name=region_name
+  )
+
+  try:
+      get_secret_value_response = client.get_secret_value(
+          SecretId=secret_name
+      )
+  except ClientError as e:
+      # For a list of exceptions thrown, see
+      # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+      raise e
+
+  # Decrypts secret using the associated KMS key.
+  secret = json.loads(get_secret_value_response['SecretString'])
+
+  USER = secret['username']
+  PASSWORD = secret['password']
+  HOST = secret['host']
+  PORT = secret['port']
+
+elif ENV == 'local':
+  USER = os.getenv('db_username')
+  PASSWORD = os.getenv('db_password')
+  HOST = os.getenv('db_host')
+  PORT = os.getenv('db_port')
+
+else:
+   raise Exception(f'Unrecognized env: {ENV}')
+
+if not USER: raise Exception('Missing required environment variable: db_username')
+if not PASSWORD: raise Exception('Missing required environment variable: db_password')
+if not HOST: raise Exception('Missing required environment variable: db_host')
+if not PORT: raise Exception('Missing required environment variable: db_port')
