9f7c5840e502bbe48278588fae8ea4d65693ac88
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-01-05 17:51:25 -0700

    made rounding getters/setters for monetary values

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 import uuid
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.types import Integer, Text, String, DateTime, Float, Boolean, Enum
 from sqlalchemy.sql import func
 
@@ -38,8 +39,24 @@ class Game(Base):
     last_modified = Column(DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
     settings_id = Column(Integer, ForeignKey("gamesettings.id"), nullable=False)
     admin_id = Column(Integer, ForeignKey("profile.id"), nullable=False)
-    total_pot = Column(Float, nullable=False, default=0)
-    available_cashout = Column(Float, nullable=False, default=0)
+    _total_pot = Column(Float, nullable=False, default=0)
+    _available_cashout = Column(Float, nullable=False, default=0)
+
+    @hybrid_property
+    def total_pot(self):
+        return self._total_pot
+
+    @total_pot.setter
+    def total_pot(self, value):
+        self._total_pot = round(value, 2)
+
+    @hybrid_property
+    def available_cashout(self):
+        return self._available_cashout
+
+    @available_cashout.setter
+    def available_cashout(self, value):
+        self._available_cashout = round(value, 2)
 
     def __repr__(self):
         return f"<Game {self.id}>"
@@ -50,11 +67,27 @@ class GameSettings(Base):
     __tablename__ = "gamesettings"
 
     id = Column(Integer, primary_key=True, autoincrement='auto', nullable=False)
-    min_buy_in = Column(Float, nullable=False)
-    max_buy_in = Column(Float, nullable=False)
+    _min_buy_in = Column(Float, nullable=False)
+    _max_buy_in = Column(Float, nullable=False)
     denominations = Column(String(255), nullable=False)
     buy_in_enabled = Column(Boolean, nullable=False, default=True)
     expired = Column(Boolean, nullable=False, default=False)
+
+    @hybrid_property
+    def min_buy_in(self):
+        return self._min_buy_in
+
+    @min_buy_in.setter
+    def min_buy_in(self, value):
+        self._min_buy_in = round(value, 2)
+
+    @hybrid_property
+    def max_buy_in(self):
+        return self._max_buy_in
+
+    @max_buy_in.setter
+    def max_buy_in(self, value):
+        self._max_buy_in = round(value, 2)
 
     def __repr__(self):
         return f"<Game Setting {self.id}>"
@@ -80,7 +113,15 @@ class Transaction(Base):
     profile_id = Column(Integer, ForeignKey("profile.id"), nullable=False)
     date = Column(DateTime, nullable=False, server_default=func.now())
     type = Column(Enum(TransactionTypes), nullable=False)
-    amount = Column(Float, nullable=False)
+    _amount = Column(Float, nullable=False)
+
+    @hybrid_property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = round(value, 2)
 
     def __repr__(self):
         return f"<Transaction {self.id}>"
